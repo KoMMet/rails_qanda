3ccf5aa0564ebf4e6598c5043e7b129873fcfa71
EC2 Default User <ec2-user@ip-172-31-43-118.ap-northeast-1.compute.internal>
2021-01-31 11:53:38 +0000

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Question.create(id: 1, name: 'TestName1', title: 'Test quest1', content: 'testcontent')
+Question.create(id: 2, name: 'TestName2', title: 'Test quest2', content: 'testcontent')
+Question.create(id: 3, name: 'TestName3', title: 'Test quest3', content: 'testcontent')
