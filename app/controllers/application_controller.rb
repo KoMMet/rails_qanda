84862ce1ff21983f138352d2a531407a47db019a
KoMMet <hipukon2@gmail.com>
2021-02-08 17:59:12 +0000

    add 500 erro page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  rescue_from StanderdError, with: :rescue500
+
+  private def rescue500(e)
+    render "errors/internal_server_error", status: 500
+  end
 end
